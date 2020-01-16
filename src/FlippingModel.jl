16cfafe7d4e4b875adee64189b2537a702482d0b
DarioSarra <dariosarra@gmail.com>
2020-01-16 10:36:42 +0000

    Files generated by PkgTemplates

---
+++ src/FlippingModel.jl
@@ -0,0 +1,5 @@
+module FlippingModel
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FlippingModel
+using Test
+
+@testset "FlippingModel.jl" begin
+    # Write your own tests here.
+end
