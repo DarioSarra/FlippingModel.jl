d65840a423a49984a4fccca3d0c3c444e5c2b737
DarioSarra <dariosarra@gmail.com>
2020-02-17 12:25:15 +0000

    AbstractModels with PoissonUniformLapse

--- src/FlippingModel.jl
+++ src/FlippingModel.jl
@@ -2,12 +2,14 @@ module FlippingModel
 
 using DataDeps, CSV
 using DataFrames, ShiftedArrays
+using Optim
 using Distributions, StructArrays, StatsBase
 
 include("datadeps.jl")
 include("loaddata.jl")
 include("bouts.jl")
 include("LogLikeliHood.jl")
+include("Models.jl")
 
 greet() = print("Hello World!")
 
--- src/LogLikeliHood.jl
+++ src/LogLikeliHood.jl
@@ -50,3 +50,54 @@ function  f_fast(cm, pars)
     v = sum(Log_LikeliHood(key..., G)*val for (key, val) in cm)
     return -v
 end
+
+abstract type AbstractModel end
+
+struct PoissonLapseUniform <: AbstractModel
+    params::Vector{Float64}
+    max::Int
+end
+function init(::Type{<:PoissonLapseUniform}, cm::Dict)
+    m = maximum(om for (om, leaves) in keys(cm))
+    return PoissonLapseUniform([10, 2, 0.1], m)
+end
+
+using Distributions: params
+Distributions.params(p::PoissonLapseUniform) = p.params
+
+function Distributions.cdf(p::PoissonLapseUniform, x::Int)
+    T, r, ϵ = params(p)
+    d = Gamma(T, 1/r)
+    # 1-ϵ e faccio Gamma, ϵ e faccio uniform
+    return (1-ϵ)*cdf(d, x) + ϵ*x/p.max
+end
+
+function Log_LikeliHood(m::AbstractModel, om, leaves)
+    if leaves
+        #= if the animal has left we want the probability of this to happen
+        at that omission, given that it has not left before.
+        Since we assume that the events are independent the product of each likelihood
+        should give me the total probability. Using the log we can simply sum them =#
+        return log(cdf(m,om) - cdf(m,om-1))
+    else
+        #= if the animal hasn't left we want the probability of not leaving until om =#
+        # this is equivalent log(1-cdf(dist,om)) but it's safe when cdf is near 1
+        return log(1-cdf(m, om))
+    end
+end
+
+function  nll_data(m::AbstractModel,cm::Dict)
+    -sum(v*Log_LikeliHood(m, k...) for (k, v) in cm)
+end
+
+function Distributions.fit(::Type{T}, b::DataFrames.AbstractDataFrame) where T<:AbstractModel
+    cm = countmap(StructArray((b.Omissions_plus_one, b.Leave)))
+    p = init(T, cm)
+    res = optimize(params(p)) do param
+        all(param .> 1e-10) || return 1e10
+        params(p) .= param
+        return nll_data(p, cm)
+    end
+    params(p) .= Optim.minimizer(res)
+    return p
+end
